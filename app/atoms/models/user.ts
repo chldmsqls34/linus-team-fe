3832b3d65d7374a1b2d272857d42a83d505eb9ab
chldmsqls34 <chldmsqls34@gmail.com>
2024-03-29 12:35:16 +0900

    feat : Add redux-counter

---
+++ app/atoms/models/user.ts
@@ -0,0 +1,12 @@
+interface IUser {
+    id: number,
+    username: string;
+    password: string;
+    repeatPassword: string;
+    name: string;
+    phone: string;
+    address: string;
+    job: string;
+    height: string;
+    weight: string;
+  }
--- app/pages/boards/articles/page.tsx
+++ app/pages/boards/articles/page.tsx
@@ -1,73 +1,31 @@
-"use client";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Box } from "@mui/material";
-import MuiDemoColumns from "@/app/organisms/columns/mui-demo-columns";
-import MuiDemoRows from "@/app/organisms/rows/mui-demo-rows";
-const SERVER = "http://localhost:8080";
+// "use client";
 
-interface IArticle{
-    id : number,
-    title : string,
-    content : string,
-    writer : string,
-    registerdate : string
-}
+// import ArticleColumns from "@/app/organisms/columns/article-columns";
+// import ArticleRows from "@/app/organisms/rows/article-rows";
+// import { Box } from "@mui/material";
+// import { DataGrid } from "@mui/x-data-grid";
+// import React from "react";
 
-export default function Articles(){
-    const[articles,setArticles]=useState<IArticle[]>([]);
 
-    const url = `${SERVER}/api/articles`
-    const config = {
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/json",
-        Authorization: "Bearer blah ~",
-        "Access-Control-Allow-Origin": "*",
-    }
-    useEffect( () => {
-      
-      axios.get(url,{headers: config})
-        .then(res => {
-          const message = res.data.message
-          console.log((message))
-          if(message=='SUCCESS'){
-            console.log("게시글이 있습니다")
-          const arr = res.data.result
-          for(const element of arr){
-            console.log(element)
-          }
-          setArticles(arr)
-          
-          }else if(message=='FAIL'){
-            console.log("게시글이 없습니다");
-          }else{
-            console.log("지정되지 않은 값");
-          }
-        })
-      },[]);
-  
-
-    return (
-    <div>
-        <h2>게시글 목록</h2>
-        <Box sx={{ height: 400, width: '100%' }}>
-    <DataGrid
-      rows={MuiDemoRows()}
-      columns={MuiDemoColumns()}
-      initialState={{
-        pagination: {
-          paginationModel: {
-            pageSize: 5,
-          },
-        },
-      }}
-      pageSizeOptions={[5]}
-      checkboxSelection
-      disableRowSelectionOnClick
-    />
-  </Box>
-      </div>)
-}
-
-   
+// export default function Articles(){
+//     const rows = ArticleRows()
+//     const columns = ArticleColumns({} as IArticle)
+//     return  (
+//         <Box sx={{ height: 400, width: '100%' }}>
+//         <DataGrid 
+//           rows={rows}
+//           columns={columns}
+//           initialState={{
+//             pagination: {
+//               paginationModel: {
+//                 pageSize: 5,
+//               },
+//             },
+//           }}
+//           pageSizeOptions={[5]}
+//           checkboxSelection
+//           disableRowSelectionOnClick
+//         />
+//         </Box>
+//       );
+// }
